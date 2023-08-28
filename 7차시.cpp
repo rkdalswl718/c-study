7c26acfc5b950ede47236e04ab4cebd6bed7b1b3
강민지 <128370710+rkdalswl718@users.noreply.github.com>
2023-08-28 14:07:17 +0900

    Update 7차시.cpp

--- 7차시.cpp
+++ 7차시.cpp
@@ -1,15 +1,23 @@
 #include <stdio.h>
-int f(int n){
-	if(n==0){
-		return 1;
-	}
-	else{
-		return f(n-1)*n;
-	}
- 
-int main (){
-	int n;
-	scanf("%d",&n);
-	printf("%d",f(n)); 
-}
 
+int main() {
+    int list[] = {5, 3, 8, 1, 2, 7};
+    int temp, index, i, j;
+
+    for (i = 0; i < sizeof(list) / sizeof(int) - 1; i++) {
+        index = i;
+        for (j = i + 1; j < sizeof(list) / sizeof(int); j++) {
+            if (list[index] > list[j]) {
+                index = j;
+            }
+        }
+        temp = list[index];
+        list[index] = list[i];
+        list[i] = temp;
+    }
+    for (i = 0; i < sizeof(list) / sizeof(int); i++) {
+        printf("%d ", list[i]);
+    }
+
+    return 0;
+}
